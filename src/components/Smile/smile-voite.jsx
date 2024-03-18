a1d1196eebad7e7bffb097c0a4de13d7dd9f06d8
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-03-18 02:06:12 +0200

    completed. add style

--- src/components/Smile/smile-voite.jsx
+++ src/components/Smile/smile-voite.jsx
@@ -1,38 +1,45 @@
-import React from "react";
-import { Component } from "react";
-import "./smile-voite.scss";
+// EmojiVote.jsx
+import React, { Component } from "react";
 
 class EmojiVote extends Component {
   constructor(props) {
     super(props);
-    const emoji = ["😬", "🥶", "🙈", "🧠", "🤠"];
     this.state = {
-      emoji,
-      votes: new Array(emoji.length).fill(0),
+      emoji: ["😬", "🥶", "🙈", "🧠", "🤠"],
+      votes: new Array(5).fill(0),
+      showResults: false,
     };
   }
+
   handleVote = (index) => {
     const newVotes = [...this.state.votes];
     newVotes[index]++;
     this.setState({ votes: newVotes });
   };
+
+  toggleResults = () => {
+    this.setState((prevState) => ({ showResults: !prevState.showResults }));
+  };
+
   render() {
+    const winningIndex = this.state.votes.indexOf(
+      Math.max(...this.state.votes)
+    );
     return (
-      <div className="headline">
+      <div>
         <h1>Vote for your favorite emoji</h1>
-        <div>
-          {this.state.emoji.map((emoji, index) => (
-            <button
-              className="smile"
-              key={index}
-              onClick={() => this.handleVote(index)}
-            >
-              {emoji} {this.state.votes[index]}
-            </button>
-          ))}
-        </div>
+        {this.state.emoji.map((emoji, index) => (
+          <button key={index} onClick={() => this.handleVote(index)}>
+            {emoji} {this.state.votes[index]}
+          </button>
+        ))}
+        <button onClick={this.toggleResults}>Show Results</button>
+        {this.state.showResults && (
+          <h2>Winning Emoji: {this.state.emoji[winningIndex]}</h2>
+        )}
       </div>
     );
   }
 }
+
 export default EmojiVote;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import EmojiVote from "./components/Smile/smile-voite";
-import ShowVoite from "./components/ShowVoite/show-voite";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <EmojiVote />
-    <ShowVoite />
   </React.StrictMode>
 );
